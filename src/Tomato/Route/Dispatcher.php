ee9a0cff7a4e49a4136bcb1b699961fd03d2a631
卢云华 <luyh@59store.com>
2016-05-30 13:26:39 +0200

    fix

---
+++ src/Tomato/Route/Dispatcher.php
@@ -0,0 +1,16 @@
+<?php
+namespace Tomato\Route;
+/**
+ * Created by luyh@59store.com.
+ * User: luyh
+ * Date: 16/5/30
+ * Time: 下午3:21
+ */
+class Dispatcher
+{
+    public function __construct()
+    {
+        echo __FUNCTION__;
+    }
+
+}
